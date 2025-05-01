d1559c4b73e07f3f412aff71e4e8e598399bd332
Vicky <rofiqiatapsil37@gmail.com>
2025-05-01 11:57:28 +0700

    awal

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,43 @@
-// /app/_layout.tsx
-import { Stack } from 'expo-router';
+import { Tabs } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function Layout() {
   return (
-    <Stack
+    <Tabs
       screenOptions={{
+        tabBarActiveTintColor: '#2196f3',
         headerStyle: { backgroundColor: '#2196f3' },
         headerTintColor: '#fff',
         headerTitleAlign: 'center',
       }}
-    />
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Beranda',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="about"
+        options={{
+          title: 'Tentang',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="information-circle" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: 'Pengaturan',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="settings" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
